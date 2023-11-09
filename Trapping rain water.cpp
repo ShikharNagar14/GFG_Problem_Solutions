cc3d3f761de7baeb62ab7d916d0e4f64442c8318
Shikhar Nagar <115800904+ShikharNagar14@users.noreply.github.com>
2023-11-09 22:22:33 +0530

    Trapping rain water.cpp

---
+++ Trapping rain water.cpp
@@ -0,0 +1,55 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+
+    public:
+    long long trappingWater(int arr[], int n){
+        // code here
+        long res=0;
+        int lmax[n],rmax[n];
+        lmax[0]=arr[0];
+        for(int i=1;i<n;i++){
+            lmax[i]=max(arr[i],lmax[i-1]);
+        }
+        rmax[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            rmax[i]=max(arr[i],rmax[i+1]);
+            
+        }
+      for(int i=1;i<n-1;i++){
+        res=res+(min(lmax[i],rmax[i])-arr[i]);
+        
+     }return res;
+    }
+};
+
+
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+      
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}
