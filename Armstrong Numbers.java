6db08901668b32e3166071a34ceef496be90b6a4
Shikhar Nagar <115800904+ShikharNagar14@users.noreply.github.com>
2023-11-12 22:07:14 +0530

    Armstrong Numbers.java

---
+++ Armstrong Numbers.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.armstrongNumber(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static String armstrongNumber(int n){
+        // code here
+        int res=0;
+        int rev=n;
+        while(n!=0){
+            int ld=n%10;
+            res=res+ld*ld*ld;
+            n=n/10;
+        }
+    
+    if(res==rev){
+        return "Yes";
+    }
+    else{
+        return "No";
+    }
+    }
+}
