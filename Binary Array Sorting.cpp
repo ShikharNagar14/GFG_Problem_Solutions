a2770fbd4bd4ea1bf516ab34abd565191de9960a
Shikhar Nagar <115800904+ShikharNagar14@users.noreply.github.com>
2023-11-16 13:55:55 +0530

    Binary Array Sorting.cpp

---
+++ Binary Array Sorting.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// A Sample C++ program for beginners with Competitive Programming
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    
+    // A[]: input array
+    // N: input array
+    //Function to sort the binary array.
+    void binSort(int arr[], int N)
+    {
+       //Your code here
+       
+       /**************
+        * No need to print the array
+        * ************/
+        int j=-1;
+        for(int i=0;i<N;i++){
+            if(arr[i]<1){
+                j++;
+                swap(arr[i],arr[j]);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int T;
+	cin>>T;
+	// Input the number of testcases
+	while(T--)
+	{
+	    int N;
+	    cin>>N; //Input size of array N
+	    int A[N]; 
+	    
+	    for(int i = 0; i  < N; i++)
+	      cin>>A[i];
+	      
+	    Solution obj;
+	    obj.binSort(A,N);
+	    
+	    for(int x:A)
+	    cout<<x<<" ";
+	      
+	    cout<<endl;
+	}
+	return 0;
+}
+
+
+
+
+// } Driver Code Ends
+// Time complexity: O(N)
+// Space complexity : O(1)
