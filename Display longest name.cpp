de807a9ce89f17fa940bde26e84bb362adbd5543
Shikhar Nagar <115800904+ShikharNagar14@users.noreply.github.com>
2023-12-11 13:30:31 +0530

    Display longest name.cpp

---
+++ Display longest name.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+        int len=0;
+        int index=0;
+       for(int i=0;i<n;i++){
+           if(names[i].length()>len){
+               index=i;
+               len=names[i].length();
+           }
+           
+       }return names[index]; 
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
